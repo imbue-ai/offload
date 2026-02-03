9ffed9c19d1cce39ca54c7b057bc19d9eb7e1684
Danver Braganza <danver@imbue.com>
2026-02-03 11:33:45 -0800

    Actually pass copy_dirs in default

--- src/provider/default.rs
+++ src/provider/default.rs
@@ -152,10 +152,23 @@ impl SandboxProvider for DefaultProvider {
         info!("Creating default sandbox: {}", config.id);
 
         // Build the create command, substituting {image_id} if available
-        let create_command = match self.image_id.as_ref() {
+        let mut create_command = match self.image_id.as_ref() {
             Some(id) => self.config.create_command.replace("{image_id}", id),
             None => self.config.create_command.clone(),
         };
+
+        for (local, remote) in &config.copy_dirs {
+            info!(
+                "  Adding --copy-dir={}:{}",
+                local.display(),
+                remote.display()
+            );
+            create_command.push_str(&format!(
+                " --copy-dir={}:{}",
+                local.display(),
+                remote.display()
+            ));
+        }
 
         // Run the create command to get a sandbox_id
         let result = self.connector.run(&create_command).await?;
